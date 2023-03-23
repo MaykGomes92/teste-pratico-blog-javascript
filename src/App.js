33517fd53cdb1bbb48485e15b62e7e75d0a7b22f
MaykGomes92 <maykcontato98@hotmail.com>
2023-03-23 18:56:37 -0300

    .

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/Header'
 import SubHeader from './components/SubHeader'
 import Posts from './components/Posts'
-import Usuarios from './components/Usuarios'
 import UsuarioSelecionado from './components/Usuarios/UsuarioSelecionado'
 import './App.css';
 
@@ -18,7 +17,6 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Posts />} />
-          <Route element={<Usuarios />} path="/usuario" />
           <Route element={<UsuarioSelecionado />} path="user/:id" />
         </Routes>
       </BrowserRouter>
